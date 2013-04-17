9fa67419366c24b1c23163b62bfbe003ec80498e
Chris Patterson <chris.patterson@relayhealth.com>
2013-04-16 17:22:22 -0700

    Cleanup

--- Demo.Tests/Consumer_Tests.cs
+++ Demo.Tests/Consumer_Tests.cs
@@ -28,22 +28,23 @@
         [TestFixtureSetUp]
         public void Setup()
         {
-            _test = TestFactory.ForConsumer<SaveMemberAddressConsumer>()
-                               .InSingleBusScenario()
-                               .New(x =>
-                                   {
-                                       x.ConstructUsing(() => new SaveMemberAddressConsumer());
-                                       x.Send(new SaveMemberAddressMessage
-                                           {
-                                               CommandId = NewId.NextGuid(),
-                                               Timestamp = DateTime.UtcNow,
-                                               MemberId = "Joe",
-                                               Street = "123 American Way",
-                                               City = "Oakland",
-                                               State = "CA",
-                                               Zip = "94602"
-                                           });
-                                   });
+            _test = TestFactory
+                .ForConsumer<SaveMemberAddressConsumer>()
+                .InSingleBusScenario()
+                .New(x =>
+                    {
+                        x.ConstructUsing(() => new SaveMemberAddressConsumer());
+                        x.Send(new SaveMemberAddressMessage
+                            {
+                                CommandId = NewId.NextGuid(),
+                                Timestamp = DateTime.UtcNow,
+                                MemberId = "Joe",
+                                Street = "123 American Way",
+                                City = "Oakland",
+                                State = "CA",
+                                Zip = "94602"
+                            });
+                    });
 
             _test.Execute();
         }
@@ -52,13 +53,13 @@
         public class SaveMemberAddressMessage :
             SaveMemberAddress
         {
-            public Guid CommandId { get;  set; }
-            public DateTime Timestamp { get;  set; }
-            public string MemberId { get;  set; }
-            public string Street { get;  set; }
-            public string City { get;  set; }
-            public string State { get;  set; }
-            public string Zip { get;  set; }
+            public Guid CommandId { get; set; }
+            public DateTime Timestamp { get; set; }
+            public string MemberId { get; set; }
+            public string Street { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Zip { get; set; }
         }
     }
 }
